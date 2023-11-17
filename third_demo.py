0a49146777b7f6d833292a0238207588dc46945e
dgroom2 <deangroom@gmail.com>
2023-11-17 12:37:37 +1100

    drop downs

--- third_demo.py
+++ third_demo.py
@@ -1,80 +1,45 @@
 import tkinter as tk
 import pandas as pd
 
-'''
-This demo shows how to load a dataset into a Pandas DataFrame and use it to populate a Listbox.
-It also shows how to use a StringVar to update the contents of a Listbox.
-It also shows how to use an OptionMenu to filter a dataset.
-It also shows how to use trace to call a function whenever a StringVar changes.
-'''
-
-# Your dataset here
 dataset = pd.DataFrame({
     'bodyPart': ['waist', 'waist', 'waist', 'upper legs', 'waist', 'back', 'lower legs', 'back'],
     'equipment': ['body weight', 'body weight', 'body weight', 'body weight', 'body weight', 'cable', 'body weight', 'body weight'],
 })
 
-# Create a new tkinter window
 window = tk.Tk()
-
-# Set the title
 window.title("Fitness Tracker")
-
-# Set the size
 window.geometry("400x400")
 
+unique_body_parts = list(dataset['bodyPart'].unique())
+unique_equipment = list(dataset['equipment'].unique())
 
-# Create a StringVar for the body part
-bodyPart = tk.StringVar(window) # Create a StringVar
-
-# Get unique body parts from the dataset
-unique_body_parts = list(dataset['bodyPart'].unique()) # Get unique body parts from the dataset
-
-#create a label for the drop down menu
 bodyPartLabel = tk.Label(window, text="Select a Body Part")
 bodyPartLabel.pack()
 
-
-
-# Create an OptionMenu for the body parts
-
-
-
-
+bodyPart = tk.StringVar(window)
+bodyPart.set("PLEASE SELECT A BODY PART")
 bodyPartMenu = tk.OptionMenu(window, bodyPart, *unique_body_parts)
 bodyPartMenu.pack()
 
-# create a label for the exercise drop down menu
 exerciseLabel = tk.Label(window, text="Select an Exercise")
-exerciseLabel.pack()# Create a Listbox for the equipment
+exerciseLabel.pack()
 
-equipmentList = tk.Listbox(window)
-equipmentList.pack()
+equipment = tk.StringVar(window)
+equipment.set(unique_equipment[0]) 
+equipmentMenu = tk.OptionMenu(window, equipment, *unique_equipment)
+equipmentMenu.pack()
 
-def update_equipment_list(*args): # *args is a special parameter that means "all the other parameters"
-
-    
-    # Get the selected body part
+def update_equipment_list(*args):
     selected_body_part = bodyPart.get() 
-
-    # Filter the dataset based on the selected body part
     filtered_dataset = dataset[dataset['bodyPart'] == selected_body_part] 
+    equipment_list = filtered_dataset['equipment'].unique()
+    
+    # Clear the current options from the equipment OptionMenu
+    equipmentMenu["menu"].delete(0, "end")
+    
+    for equip in equipment_list:
+        equipmentMenu["menu"].add_command(label=equip, command=tk._setit(equipment, equip))
 
-    # Get the corresponding equipment
-    equipment = filtered_dataset['equipment'].unique()
+bodyPart.trace('w', update_equipment_list)
 
-    # Clear the Listbox
-    equipmentList.delete(0, tk.END) # Delete everything from index 0 to the end
-
-    # Insert the equipment into the Listbox
-    for item in equipment: # Iterate through the equipment
-        equipmentList.insert(tk.END, item) # Insert each item into the Listbox until the end
-
-# Call update_equipment_list whenever the selected body part changes
-bodyPart.trace('w', update_equipment_list) # 'w' means "whenever the StringVar changes"
-
-# Set the initial body part
-bodyPart.set(unique_body_parts[0]) # Set the initial body part to the first item in the list
-
-# Start the tkinter main loop
 window.mainloop()
